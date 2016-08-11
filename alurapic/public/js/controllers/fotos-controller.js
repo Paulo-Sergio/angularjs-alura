250c51f8d5295bbc473dcb83248b06bb2e7dd7a9
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-08-11 18:39:39 -0300

    aula05 - aplicando recurso de filtro no projeto e trabalhando com ngAnimate

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,8 +1,9 @@
 angular.module('alurapic').controller('FotosController', function ($scope, $http) {
 
-    $scope.fotos = []
+    $scope.fotos = [];
+    $scope.filtro = '';
 
-//1ª FORMA 
+//1ª FORMA [LER DADOS WEB-SERVICE]
 //    var promise = $http.get('v1/fotos');
 //    promise.then(function(dados) {
 //        $scope.fotos = dados.data;
@@ -10,7 +11,7 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
 //        console.log(error);
 //    });
 
-//2ª FORMA (mais usado eu acho)
+//2ª FORMA (mais usado eu acho) [LER DADOS WEB-SERVICE]
 //    $http.get('v1/fotos')
 //            .then(function (dados) {
 //                $scope.fotos = dados.data;
@@ -18,7 +19,7 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
 //                console.log(err);
 //            });
 
-//3ª FORMA (mais simplificada)
+//3ª FORMA (mais simplificada) [LER DADOS WEB-SERVICE]
     $http.get('v1/fotos')
             .success(function (fotos) {
                 $scope.fotos = fotos;
