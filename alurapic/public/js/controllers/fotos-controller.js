5b93385835bbafb6eeb70e45372b1156ecdc6921
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-08-11 15:04:05 -0300

    aula 03 - trabalhando com $http no controller para consumir dados e ng-repeat para exibir pela view

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,8 +1,30 @@
-angular.module('alurapic').controller('FotosController', function($scope){
-    
-    $scope.foto = {
-        titulo: "Leão",
-        url: "http://www.fundosanimais.com/Minis/leoes.jpg"
-    };
-    
+angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+
+    $scope.fotos = []
+
+//1ª FORMA 
+//    var promise = $http.get('v1/fotos');
+//    promise.then(function(dados) {
+//        $scope.fotos = dados.data;
+//    }).catch(function(error){
+//        console.log(error);
+//    });
+
+//2ª FORMA (mais usado eu acho)
+//    $http.get('v1/fotos')
+//            .then(function (dados) {
+//                $scope.fotos = dados.data;
+//            }, function(err){
+//                console.log(err);
+//            });
+
+//3ª FORMA (mais simplificada)
+    $http.get('v1/fotos')
+            .success(function (fotos) {
+                $scope.fotos = fotos;
+            })
+            .error(function (err) {
+                console.log(err);
+            });
+
 });
