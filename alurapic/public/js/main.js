b68aeefadd099f8c28972687477204cd8ba60ac2
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-08-11 19:11:26 -0300

    aula06 - separando html em partials, utilizando sistema de rotas do angular injetando[ngRoute]

--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,1 +1,21 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+        .config(function ($routeProvider, $locationProvider) {
+
+            /* para suportar a url sem #
+             * html5Mode(false): http://localhost:3000/#/fotos
+             * html5Mode(true): http://localhost:3000/fotos
+             */
+            $locationProvider.html5Mode(true);
+
+            $routeProvider.when('/fotos', {
+                templateUrl: 'partials/principal.html',
+                controller: 'FotosController'
+            });
+            
+            $routeProvider.when('/fotos/new', {
+                templateUrl: 'partials/foto.html'
+            });
+            
+            $routeProvider.otherwise('/fotos');
+
+        });
