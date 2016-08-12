484326168684c5e5f791edb2fe8803b9bad0cd3a
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-08-12 12:20:21 -0300

    aula07 pt.2 - validação de form com angularjs, diretiva (<minha-foto>), $http.post, mensagens de validação e etc...

--- alurapic/public/js/controllers/foto-controller.js
+++ alurapic/public/js/controllers/foto-controller.js
@@ -1,9 +1,23 @@
-angular.module('alurapic').controller('FotoController', function ($scope){
-    
+angular.module('alurapic').controller('FotoController', function ($scope, $http) {
+
     $scope.foto = {};
-    
-    $scope.submeter = function(){
-        console.log($scope.foto);
+    $scope.mensagem = '';
+
+    $scope.submeter = function () {
+        if($scope.formulario.$valid){
+            
+            console.log($scope.foto);
+            $http.post('v1/fotos', $scope.foto)
+                    .success(function () {
+                        $scope.foto = {};
+                        $scope.mensagem = "Foto incluida com sucesso";
+                    })
+                    .error(function (err) {
+                        $scope.mensagem = "Não foi possivel incluir a foto";
+                        console.log(err);
+                    });  
+        }
+        
     };
-    
+
 });
--- alurapic/public/js/directives/minhas-diretivas.js
+++ alurapic/public/js/directives/minhas-diretivas.js
@@ -1,18 +1,32 @@
-angular.module('minhasDiretivas', []).directive('meuPainel', function(){
-    
+angular.module('minhasDiretivas', []).directive('meuPainel', function () {
+
     //ddo = direct definition object
     var ddo = {};
-    
+
     ddo.restrict = "AE"; // A = atributte, E = element
-    ddo.scope = { 
+    ddo.scope = {
         //passando dado para dentro do scopo privado da diretiva
         titulo: '@titulo'
     };
-    
+
     ddo.transclude = true; //para diretiva manter os elementos filhos
-    
+
     ddo.templateUrl = 'js/directives/meu-painel.html';
-    
+
     return ddo;
-    
+
+}).directive('minhaFoto', function () {
+
+    var ddo = {};
+
+    ddo.restrict = "AE";
+
+    ddo.scope = {
+        titulo: '@',
+        url: '@'
+    };
+
+    ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';
+
+    return ddo;
 });
